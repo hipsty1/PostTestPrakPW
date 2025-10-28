ceeec54386a8305ab7347905096c817200a30f80
hipsty1 <123240066@student.upnyk.ac.id>
2025-10-28 22:50:26 +0700

    finall

--- cek.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_COOKIE['nama'])) {
-    echo "Cookie telah disimpan. Nama: " . $_COOKIE['nama'];
-} else {
-    echo "Cookie belum tersedia (mungkin sudah kadaluarsa).";
-}
-?>
--- cookie.php
+++
@@ -1,5 +0,0 @@
-<?php
-// HARUS di baris pertama sebelum ada echo/HTML
-setcookie('nama', 'Bahlil', time() + 6); // berlaku 60 detik
-echo "Cookie 'nama' telah dibuat. Silakan buka cek.php dalam 6 detik.";
-?>
